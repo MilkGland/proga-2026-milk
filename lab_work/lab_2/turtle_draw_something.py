c2203c662482df5f44f63adbb0d8d3820a767fbb
MilkGland <alekseyrediskin@gmail.com>
2026-04-21 21:20:19 +0800

    Made documentation for function, refactored code, and added ability to set size of circle in 'draw_circle' function; optimized 'draw_ten_nested_square' function, added ability to set count of shapes, and changed its name to 'draw_nested_square'

--- lab_work/lab_2/turtle_draw_something.py
+++ lab_work/lab_2/turtle_draw_something.py
@@ -1,31 +1,52 @@
 import turtle
-turtle.speed(1)
+
 turtle.shape('turtle')
 
+
 def draw_regular_polygon(count_side, length):
+    """
+    Функция, рисующая любой правильный многоугольник
+    :param count_side: количество углов многоугольника
+    :param length: длина стороны многоугольника
+    :return: None
+    """
     for side in range(count_side):
         turtle.forward(length)
-        turtle.right(360 // count_side)
+        turtle.left(360 // count_side)
 
 
-def draw_circle():
-    for radius in range(360 // 5):
-        turtle.forward(5)
-        turtle.right(5)
+def draw_circle(size):
+    #FIXME
+    """
+    Функция, рисующая окружность, которая представляет из себя многоугольник с множеством углов
+    :param size: диаметр окружности
+    :return: None
+    """
+    for _ in range(360 // 5): # Определение градусной меры окружности с учетом фиксированого параметра
+        turtle.forward(size)
+        turtle.right(5) # Фиксированый параметр, влияющий на количество углов в данном многоугольнике
+                        # Чем он больше, тем меньше углов в многоугольнике
+                        # Чем он меньше, тем медленнее черепаха рисует многоугольник
 
 
-def draw_ten_nested_square(length):
-    for square in range(10):
+def draw_nested_square(count_square, length, indent):
+    #FIXME
+    """
+    Функция, рисующая произвольное количество вложеных правильных квадратов
+    :param count_square: количество квадратов
+    :param length: длина стороны квадрата
+    :param indent: размер отступа между квадратами
+    :return: None
+    """
+    x = indent # Координата перемещения черепхи по оси X
+    y = indent # Координата перемещения черепхи по оси Y
+    for square in range(count_square):
         draw_regular_polygon(4, length)
-        length += 10
+        length += indent * 2
 
         turtle.penup()
-        turtle.left(90)
-        turtle.forward(5)
-        turtle.right(90)
-        turtle.back(5)
+        turtle.goto(-x, -y)
+        x += indent
+        y += indent
         turtle.pendown()
 
-
-
-draw_ten_nested_square(50)

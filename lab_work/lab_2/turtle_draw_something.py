67312fa055a35da0b6443a9b4b260261632b3373
MilkGland <alekseyrediskin@gmail.com>
2026-04-22 17:10:34 +0800

    Described 'draw_spider' function and documented its interface

--- lab_work/lab_2/turtle_draw_something.py
+++ lab_work/lab_2/turtle_draw_something.py
@@ -3,44 +3,42 @@ import turtle
 turtle.shape('turtle')
 
 
-def draw_regular_polygon(count_side, length):
+def draw_regular_polygon(quantity_side, length):
     """
     Функция, рисующая любой правильный многоугольник
-    :param count_side: количество углов многоугольника
+    :param quantity_side: количество углов многоугольника
     :param length: длина стороны многоугольника
     :return: None
     """
-    for side in range(count_side):
+    for side in range(quantity_side):
         turtle.forward(length)
-        turtle.left(360 // count_side)
+        turtle.left(360 // quantity_side)
 
 
-def draw_circle(size):
-    #FIXME
+def draw_circle(length):
     """
     Функция, рисующая окружность, которая представляет из себя многоугольник с множеством углов
-    :param size: диаметр окружности
+    :param length: длина окружности
     :return: None
     """
-    for _ in range(360 // 5): # Определение градусной меры окружности с учетом фиксированого параметра
-        turtle.forward(size)
+    for polygon in range(360 // 5): # Определение длины окружности с учетом фиксированого параметра
+        turtle.forward(length)
         turtle.right(5) # Фиксированый параметр, влияющий на количество углов в данном многоугольнике
                         # Чем он больше, тем меньше углов в многоугольнике
                         # Чем он меньше, тем медленнее черепаха рисует многоугольник
 
 
-def draw_nested_square(count_square, length, indent):
-    #FIXME
+def draw_nested_square(number_of_square, length, indent=5):
     """
     Функция, рисующая произвольное количество вложеных правильных квадратов
-    :param count_square: количество квадратов
+    :param number_of_square: количество квадратов
     :param length: длина стороны квадрата
     :param indent: размер отступа между квадратами
     :return: None
     """
     x = indent # Координата перемещения черепхи по оси X
     y = indent # Координата перемещения черепхи по оси Y
-    for square in range(count_square):
+    for square in range(number_of_square):
         draw_regular_polygon(4, length)
         length += indent * 2
 
@@ -50,3 +48,19 @@ def draw_nested_square(count_square, length, indent):
         y += indent
         turtle.pendown()
 
+
+def draw_spider(number_of_legs, length):
+    """
+    Функция, рисующая произвольное количество ног паучку (лучей)
+    :param number_of_legs: количество исходящих из центра лучей, которые представляют ноги паука
+    :param length: длина ног (лучей)
+    :return: None
+    """
+    for leg in range(number_of_legs):
+        turtle.forward(length)
+        turtle.stamp()
+        turtle.right(360 // number_of_legs) # Определение величины угла между лучами
+        turtle.goto(0, 0)
+
+
+draw_spider(12, 100)

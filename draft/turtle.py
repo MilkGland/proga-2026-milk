59b84cba6f2a5903127a05f35e1e616e2d622e64
MilkGland <alekseyrediskin@gmail.com>
2026-05-19 14:42:00 +0800

    Create new directory

---
+++ draft/turtle.py
@@ -0,0 +1,15 @@
+import turtle as tr
+
+tr.left(54)
+
+for _ in range(5):
+    tr.forward(4)
+    tr.left(72)
+    tr.forward(4)
+    tr.left(72*2)
+
+tr.left(36)
+
+for _ in range(5):
+    tr.forward(4)
+    tr.left(72)

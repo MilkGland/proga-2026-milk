b27f0fe41054fad943a9b4456d69422c481abb48
MilkGland <alekseyrediskin@gmail.com>
2026-06-20 18:25:30 +0800

    Fix code

--- lab_work/lab_3/postal_code.py
+++ lab_work/lab_3/postal_code.py
@@ -5,24 +5,13 @@ turtle.speed(5)
 turtle.pensize(5)
 
 
-def _window_parameter(code: list, side: int):
-    #FIXME
-    #TODO: right window size
+def _window_parameter(code: tuple, side: int):
+    #TODO: indent to window size
 
-    display_resolution = {'width': 1920, 'height': 1080}
-    window_resolution = (display_resolution['width'] * 0.5,
-                         display_resolution['height'] * 0.75)
-
-    text_width = len(code) * side*2 - side
-    text_height = side * 2
-
-    width_indentation = (window_resolution[0] - text_width) / 2
-    height_indentation = (window_resolution[1] - text_height) / 2
-
-    return width_indentation, height_indentation
+    return 250
 
 
-def draw_code(code: list):
+def draw_code(code: tuple):
     """
     Функция, рисующая цифры почтового индекса по
     переданному списку чисел
@@ -35,128 +24,129 @@ def draw_code(code: list):
 
     indent = _window_parameter(code, side)
 
-    x = -indent[0]
+    x = -indent
     y = 0
 
-    for elements in code:
-        turtle.penup()
-        turtle.goto(x, y) # Перемещение в точку начала рисования
-        turtle.pendown()
-
-        x += side * 2 # Отступ между числами
-
-        if elements == 0:
-            for _ in range(2):
-                turtle.forward(side)
-                turtle.right(90)
-                turtle.forward(side * 2)
-                turtle.right(90)
-
-        elif elements == 1:
+    for tuples in code:
+        for elements in tuples:
             turtle.penup()
-            turtle.right(90)
-            turtle.forward(side)
+            turtle.goto(x, y) # Перемещение в точку начала рисования
             turtle.pendown()
 
-            turtle.left(90 + 45)
-            turtle.forward(diagonal)
-            turtle.right(45 + 90)
-            turtle.forward(side * 2)
+            x += side * 2 # Отступ между числами
 
-            turtle.left(90) # Исходное положение
+            if elements == 0:
+                for _ in range(2):
+                    turtle.forward(side)
+                    turtle.right(90)
+                    turtle.forward(side * 2)
+                    turtle.right(90)
 
-        elif elements == 2:
-            turtle.forward(side)
-            turtle.right(90)
-            turtle.forward(side)
-            turtle.right(45)
-            turtle.forward(diagonal)
-            turtle.left(45 + 90)
-            turtle.forward(side)
-
-        elif elements == 3:
-            for _ in range(2):
+            elif elements == 1:
+                turtle.penup()
+                turtle.right(90)
                 turtle.forward(side)
+                turtle.pendown()
+
+                turtle.left(90 + 45)
+                turtle.forward(diagonal)
+                turtle.right(45 + 90)
+                turtle.forward(side * 2)
+
+                turtle.left(90) # Исходное положение
+
+            elif elements == 2:
+                turtle.forward(side)
+                turtle.right(90)
+                turtle.forward(side)
+                turtle.right(45)
+                turtle.forward(diagonal)
+                turtle.left(45 + 90)
+                turtle.forward(side)
+
+            elif elements == 3:
+                for _ in range(2):
+                    turtle.forward(side)
+                    turtle.right(45 + 90)
+                    turtle.forward(diagonal)
+                    turtle.left(45 + 90)
+
+            elif elements == 4:
+                turtle.right(90)
+                turtle.forward(side)
+
+                for _ in range(2):
+                    turtle.left(90)
+                    turtle.forward(side)
+
+                turtle.right(90 + 90)
+                turtle.forward(side * 2)
+
+                turtle.left(90) # Исходное положение
+
+            elif elements == 5:
+                turtle.forward(side)
+                turtle.right(90 + 90)
+                turtle.forward(side)
+
+                for _ in range(2):
+                    turtle.left(90)
+                    turtle.forward(side)
+
+                for _ in range(2):
+                    turtle.right(90)
+                    turtle.forward(side)
+
+                turtle.right(90 + 90) # Исходное положение
+
+            elif elements == 6:
+                turtle.penup()
+                turtle.forward(side)
+                turtle.pendown()
+
                 turtle.right(45 + 90)
                 turtle.forward(diagonal)
                 turtle.left(45 + 90)
 
-        elif elements == 4:
-            turtle.right(90)
-            turtle.forward(side)
+                for _ in range(4):
+                    turtle.forward(side)
+                    turtle.right(90)
 
-            for _ in range(2):
+            elif elements == 7:
+                turtle.forward(side)
+                turtle.right(45 + 90)
+                turtle.forward(diagonal)
+                turtle.left(45)
+                turtle.forward(side)
+
+                turtle.left(90) # Исходное положение
+
+            elif elements == 8:
+                for _ in range(4):
+                    turtle.forward(side)
+                    turtle.right(90)
+
+                turtle.right(90)
+                turtle.forward(side)
+                turtle.left(90)
+
+                for _ in range(4):
+                    turtle.forward(side)
+                    turtle.right(90)
+
+            elif elements == 9:
+                for _ in range(4):
+                    turtle.forward(side)
+                    turtle.right(90)
+
+                turtle.right(90)
+                turtle.forward(side)
                 turtle.left(90)
                 turtle.forward(side)
+                turtle.right(45 + 90)
+                turtle.forward(diagonal)
 
-            turtle.right(90 + 90)
-            turtle.forward(side * 2)
-
-            turtle.left(90) # Исходное положение
-
-        elif elements == 5:
-            turtle.forward(side)
-            turtle.right(90 + 90)
-            turtle.forward(side)
-
-            for _ in range(2):
-                turtle.left(90)
-                turtle.forward(side)
-
-            for _ in range(2):
-                turtle.right(90)
-                turtle.forward(side)
-
-            turtle.right(90 + 90) # Исходное положение
-
-        elif elements == 6:
-            turtle.penup()
-            turtle.forward(side)
-            turtle.pendown()
-
-            turtle.right(45 + 90)
-            turtle.forward(diagonal)
-            turtle.left(45 + 90)
-
-            for _ in range(4):
-                turtle.forward(side)
-                turtle.right(90)
-
-        elif elements == 7:
-            turtle.forward(side)
-            turtle.right(45 + 90)
-            turtle.forward(diagonal)
-            turtle.left(45)
-            turtle.forward(side)
-
-            turtle.left(90) # Исходное положение
-
-        elif elements == 8:
-            for _ in range(4):
-                turtle.forward(side)
-                turtle.right(90)
-
-            turtle.right(90)
-            turtle.forward(side)
-            turtle.left(90)
-
-            for _ in range(4):
-                turtle.forward(side)
-                turtle.right(90)
-
-        elif elements == 9:
-            for _ in range(4):
-                turtle.forward(side)
-                turtle.right(90)
-
-            turtle.right(90)
-            turtle.forward(side)
-            turtle.left(90)
-            turtle.forward(side)
-            turtle.right(45 + 90)
-            turtle.forward(diagonal)
-
-            turtle.left(45 + 90) # Исходное положение
+                turtle.left(45 + 90) # Исходное положение
 
 
-draw_code([1, 4, 1, 7, 0, 0])
+draw_code([(1, 4, 1, 7, 0, 0), (5, 8, 9, 9, 2, 1)])
